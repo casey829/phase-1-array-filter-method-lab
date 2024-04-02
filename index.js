c5c3daeea62cfbe8fc36ffb08f299384bd982a69
casey <caseymwangi31@gmail.com>
2024-04-02 07:43:28 +0300

    understanding filter method

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(drivers,sameName) {
+    return drivers.filter((driverElement) => driverElement.toLowerCase().includes(sameName.toLowerCase()));
+}
+
+
+function fuzzyMatch(drivers,letterMatch) {
+    return drivers.filter((driverElement) => driverElement.toLowerCase().startsWith(letterMatch.toLowerCase()));
+}
+
+
+function matchName(drivers,nameMatch) {
+    return drivers.filter((driverElement) => driverElement.name.includes(nameMatch));
+}
